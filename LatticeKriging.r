a7e6b455226d42492296eef4f567627541359650
hzfywhn <hzfywhn@users.noreply.github.com>
2020-10-20 00:38:56 -0400

    Rewrite LatticeKriging
    Rewrite LatticeKriging following the same notation as in Nychka et al 2015

--- LatticeKriging.r
+++ LatticeKriging.r
@@ -1,163 +1,187 @@
 # a simplified rewrite of the original LatticeKrig package on Cartesian geometry
-# no standard error estimate yet, implementing
 
 library(package = "spam")
 
-LKfit <- function(lambda, wy, wU, wX, Gchol) {
-    # calculate coefficients
-    A <- t(wU) %*% (wU - wX %*% backsolve(Gchol, forwardsolve(Gchol, t(wX) %*% wU))) / lambda
-    b <- t(wU) %*% (wy - wX %*% backsolve(Gchol, forwardsolve(Gchol, t(wX) %*% wy))) / lambda
-    d <- solve(A) %*% b
-    r <- wy - wU %*% d
-    c <- forwardsolve(Gchol, t(wX) %*% r)
-    rhoMLE <- (sum(r^2) - sum(c^2)) / lambda / length(wy)
-    c <- backsolve(Gchol, c)
-    return (list(d = d, c = c, rhoMLE = rhoMLE))
-}
-
-LK <- function(lambda, wy, wU, wX, Q, Qchol, rho, weights, reps) {
-    # LK for fixed lambda
-
-    n <- length(wy)
-    m <- nrow(Q)
-
-    G <- t(wX) %*% wX + lambda * Q
-
-    # pivoting can be turned on depending on performance
-    Gchol <- chol(G, pivot = FALSE)
-
-    LKfit_res <- LKfit(lambda, wy, wU, wX, Gchol)
-    residual <- (wy - (wU %*% LKfit_res$d + wX %*% LKfit_res$c)) / sqrt(weights)
-
-    # likelihood
-    lnDetCov <- 2*sum(log(diag(Gchol))) - 2*sum(log(diag(Qchol))) + (n-m)*log(lambda) - sum(log(weights))
-    lnProfileLike <- -n/2 - n/2*log(2*pi) - n/2*log(LKfit_res$rhoMLE) - 1/2*lnDetCov
-    lnLike <- -LKfit_res$rhoMLE*n/(2*rho) - n/2*log(2*pi) - n/2*log(rho) - 1/2*lnDetCov
-
-    est <- NA
-    SE <- NA
-    GCV <- NA
-    if (reps >= 1) {
-        wEy <- matrix(data = rnorm(reps * n), nrow = n, ncol = reps) * sqrt(weights)
-        trAest <- LKfit(lambda, wEy, wU, wX, Gchol)
-        wEyhat <- wU %*% trAest$d + wX %*% trAest$c
-        info <- colSums(wEy * wEyhat / weights)
-        est <- mean(info)
-        SE <- sqrt(var(info) / length(info))
-        GCV <- sum(weights * residual^2) / n / (1 - est/n)^2
+SAR <- function(basis) {
+    # spatial autogression
+    m <- nrow(basis$loc)
+    B <- spam(x = 0, nrow = m, ncol = m)
+    for (j in 1: m) {
+        B[j, j] <- basis$centerweight
+        con <- basis$connect[j, ]
+        B[j, con[!is.na(con)]] <- -1
     }
-
-    return (c(LKfit_res, list(lnProfileLike = lnProfileLike, lnLike = lnLike, est = est, SE = SE, GCV = GCV)))
+    Bs <- B / sqrt(basis$alpha)
+    return (Bs)
 }
 
-LKfixed <- function(loc, Z) {
-    # include possible covariates
-    return (cbind(1, loc, Z))
+precision <- function(basis, rho) {
+    B <- SAR(basis)
+    Q <- t(B) %*% B / rho
+    return (Q)
 }
 
-LKbasis <- function(obsloc, basis, rho, normalize, Qchol) {
-    # pairwise distance and basis evaluation (phi matrix)
+regression <- function(loc, basis) {
+    n <- nrow(loc)
+    m <- nrow(basis$loc)
+    delta2 <- basis$delta^2
+    dist <- spam(x = 0, nrow = n, ncol = m)
+    # pairwise distance
+    for (i in 1: n) {
+        for (j in 1: m) {
+            dist2 <- sum((loc[i, ] - basis$loc[j, ])^2)
+            if (dist2 <= delta2) dist[i, j] <- sqrt(dist2)
+        }
+    }
+    d <- dist / basis$delta
+    phi <- (1 - d)^6 * (35*d^2 + 18*d + 3) / 3
+    return (phi)
+}
 
-    n <- nrow(obsloc)
+combineMR <- function(loc, basis, normalize, rho) {
+    n <- nrow(loc)
     nlev <- length(basis)
 
-    mall <- 0
+    m0 <- array(dim = nlev)
     for (ilev in 1: nlev)
-        mall <- mall + nrow(basis[[ilev]]$loc)
-    Xall <- spam(x = 0, nrow = n, ncol = mall)
+        m0[ilev] <- nrow(basis[[ilev]]$loc)
+    m <- cumsum(c(0, m0))
 
-    mall <- 0
+    phi <- spam(x = 0, nrow = n, ncol = m[nlev+1])
+    Qc <- spam(x = 0, nrow = m[nlev+1], ncol = m[nlev+1])
     for (ilev in 1: nlev) {
-        m <- nrow(basis[[ilev]]$loc)
+        start_index <- m[ilev] + 1
+        end_index <- m[ilev+1]
 
-        distance <- spam(x = 0, nrow = n, ncol = m)
-        for (i in 1: n) {
-            for (j in 1: m) {
-                dist2 <- sum((obsloc[i, ] - basis[[ilev]]$loc[j, ])^2)
-                if (dist2 <= basis[[ilev]]$delta^2) distance[i, j] <- sqrt(dist2) / basis[[ilev]]$delta
-            }
-        }
-        phi <- (1 - distance)^6 * (35*distance^2 + 18*distance + 3) / 3
-
-        X <- sqrt(basis[[ilev]]$alpha) * phi
+        Q <- precision(basis[[ilev]], rho)
+        Qc0 <- chol(Q, pivot = FALSE)
+        phi0 <- regression(loc, basis[[ilev]])
 
         if (normalize) {
-            normweight <- colSums(forwardsolve(Qchol[[ilev]], t(phi))^2)
-            normweight[normweight == 0] <- 1
-            X <- diag.spam(1/sqrt(normweight)) %*% X
+            # still don't understand how it is related to Sec 2.6
+            normweight <- rho * colSums(forwardsolve(Qc0, t(phi0))^2)
+            stopifnot(normweight != 0)
+            phi0 <- diag.spam(1/sqrt(normweight)) %*% phi0
         }
 
-        Xall[, (mall+1): (mall+m)] <- X
-        mall <- mall + m
+        phi[, start_index: end_index] <- phi0
+        Qc[start_index: end_index, start_index: end_index] <- as.spam(Qc0)
     }
 
-    return (sqrt(rho) * Xall)
+    return (list(phi = phi, Qc = Qc))
 }
 
-LatticeKriging <- function(obs, basis, Z, rho, normalize, reps) {
-    # entrance of LK, optimize with lambda
+covariate <- function(loc) {
+    Z <- cbind(1, loc)
+    return (Z)
+}
 
+errorCov <- function(err) {
+    W <- diag.spam(x = 1/err^2)
+    return (W)
+}
+
+LatticeKriging <- function(obs, basis, normalize, lambda, rho) {
     n <- nrow(obs$loc)
-    nlev <- length(basis)
 
-    # need to verify
-    weights <- 1 / obs$err^2
+    MR <- combineMR(obs$loc, basis, normalize, rho)
+    Qinv <- chol2inv(MR$Qc)
 
-    wy <- sqrt(weights) * obs$val
+    Z <- covariate(obs$loc)
+    y <- obs$val
+    W <- errorCov(obs$err)
 
-    # fixed part
-    wU <- sqrt(weights) * LKfixed(obs$loc, Z)
+    M <- MR$phi %*% Qinv %*% t(MR$phi) + lambda * chol2inv(chol(W, pivot = FALSE))
 
-    # precision matrix
-    mall <- 0
-    for (ilev in 1: nlev)
-        mall <- mall + nrow(basis[[ilev]]$loc)
-    Qall <- spam(x = 0, nrow = mall, ncol = mall)
-    Qchol <- vector(mode = "list", length = nlev)
-    Qcholall <- spam(x = 0, nrow = mall, ncol = mall)
+    # here is the most expansive computation taken place
+    Mc <- chol(M, pivot = FALSE)
+    Minv <- chol2inv(Mc)
+    tZMinvZinv <- chol2inv(chol(t(Z) %*% Minv %*% Z, pivot = FALSE))
 
-    mall <- 0
-    for (ilev in 1: nlev) {
-        m <- nrow(basis[[ilev]]$loc)
+    d <- tZMinvZinv %*% t(Z) %*% Minv %*% y
+    r <- y - Z %*% d
+    c <- Qinv %*% t(MR$phi) %*% Minv %*% r
+    rhoMLE <- t(r) %*% Minv %*% r / n
+    likelihood <- -n/2 - n/2*log(rhoMLE) - sum(log(diag(Mc)))
+    # discrepancy happens, the last term is +n/2*log(pi) in Eq 7
+    likelihood <- likelihood - n/2*log(2*pi)
 
-        B <- spam(x = 0, nrow = m, ncol = m)
-        for (j in 1: m) {
-            B[j, j] <- basis[[ilev]]$centerweight
-            con <- basis[[ilev]]$connect[j, ]
-            B[j, con[!is.na(con)]] <- -1
-        }
-        Q <- t(B) %*% B
+    # collapse 1x1 matrix to scalar
+    rhoMLE <- rhoMLE[1, 1]
+    return (c(MR, list(Qinv = Qinv, Z = Z, W = W, d = d, c = c, rhoMLE = rhoMLE,
+        likelihood = likelihood, Minv = Minv, tZMinvZinv = tZMinvZinv)))
+}
 
-        # disable pivoting for matrix assignment
-        Qchol[[ilev]] <- chol(Q, pivot = FALSE)
+predictLK <- function(obs, basis, loc, lambda, normalize, rho) {
+    fit <- LatticeKriging(obs, basis, normalize, lambda, rho)
 
-        Qall[(mall+1): (mall+m), (mall+1): (mall+m)] <- Q
-        Qcholall[(mall+1): (mall+m), (mall+1): (mall+m)] <- as.spam(Qchol[[ilev]])
-        mall <- mall + m
+    Z1 <- covariate(loc)
+    phi1 <- combineMR(loc, basis, normalize, rho)$phi
+
+    mean <- Z1 %*% fit$d + phi1 %*% fit$c
+
+    # standard deviation prediction is confusing with no reference found, need verification
+    y1 <- fit$phi %*% fit$Qinv %*% t(phi1)
+    d1 <- fit$tZMinvZinv %*% t(fit$Z) %*% fit$Minv %*% y1
+    r1 <- y1 - fit$Z %*% d1
+    c1 <- fit$Qinv %*% t(fit$phi) %*% fit$Minv %*% r1
+    resid <- y1 - fit$Z %*% d1 - fit$phi %*% c1
+    joint <- colSums(t(Z1) * (fit$tZMinvZinv %*% t(Z1))) - 2 * colSums(t(Z1) * d1)
+    # seems like only diagonals of error covariance matrix is used in marginal variance calculation
+    marginal <- rho * colSums(forwardsolve(fit$Qc, t(phi1))^2) - colSums(y1*resid) / lambda
+    sd <- sqrt(fit$rhoMLE * abs(joint + marginal))
+
+    return (list(mean = mean, sd = sd))
+}
+
+
+# codes below are for testing
+
+# testing domain
+xylim <- c(-2, 2)
+
+# fake observation
+xy <- seq(from = xylim[1], to = xylim[2], by = 0.5)
+coor <- expand.grid(xy, xy)
+x <- coor[, 1]
+y <- coor[, 2]
+z <- x^2 + y^2
+obs <- list(loc = cbind(x, y), val = z, err = rep(1, times = length(z)))
+
+# distance between basis functions
+delta <- 1
+xy <- seq(from = xylim[1], to = xylim[2], by = delta)
+nc <- length(xy)
+
+coor <- expand.grid(xy, xy)
+x <- coor[, 1]
+y <- coor[, 2]
+
+# constructing connectivity matrix is simple when the whole domain is filled
+connect <- array(dim = c(nc^2, 4))
+for (i in 1: nc) {
+    for (j in 1: nc) {
+        idx <- (i-1)*nc + j
+        if (i >= 2) connect[idx, 1] <- (i-2)*nc + j
+        if (j >= 2) connect[idx, 2] <- (i-1)*nc + j-1
+        if (i <= nc-1) connect[idx, 3] <- i*nc + j
+        if (j <= nc-1) connect[idx, 4] <- (i-1)*nc + j+1
     }
-
-    # random part
-    X <- LKbasis(obs$loc, basis, rho, normalize, Qchol)
-    wX <- diag.spam(sqrt(weights)) %*% X
-
-    # main loop for optimization
-    opt <- optimize(
-        function(loglambda) {
-            LK_res <- LK(exp(loglambda), wy, wU, wX, Qall, Qcholall, rho, weights, 0)
-            return (LK_res$lnProfileLike)
-        }, interval = c(-9, 4), maximum = TRUE, tol = 5e-3)
-
-    lambda <- exp(opt$maximum)
-    LK_res <- LK(lambda, wy, wU, wX, Qall, Qcholall, rho, weights, reps)
-
-    return (c(LK_res, list(Qchol = Qchol, lambda = lambda)))
 }
+basis1 <- list(loc = cbind(x, y), connect = connect, centerweight = 4.01, delta = delta*2.5, alpha = 1)
+basis <- list(basis1)
 
-predictMean <- function(loc, basis, Z, rho, normalize, Qchol, d, c) {
-    return (LKfixed(loc, Z) %*% d + LKbasis(loc, basis, rho, normalize, Qchol) %*% c)
-}
+normalize <- TRUE
+rho <- 1
+lambda <- exp(optimize(
+    function(loglambda) {
+        fit <- LatticeKriging(obs, basis, normalize, exp(loglambda), rho)
+        return (fit$likelihood)
+    }, interval = c(-9, 4), maximum = TRUE, tol = 5e-3)$maximum)
+fit <- LatticeKriging(obs, basis, normalize, lambda, rho)
 
-predictSD <- function() {
-    # to be completed
-    return ()
-}
+xy <- seq(from = xylim[1], to = xylim[2], by = 0.2)
+coor <- expand.grid(xy, xy)
+x <- coor[, 1]
+y <- coor[, 2]
+LK <- predictLK(obs, basis, cbind(x, y), lambda, normalize, rho)
